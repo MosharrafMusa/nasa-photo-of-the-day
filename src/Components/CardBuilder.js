a8193c20723c936478173755393fa03877a80442
Mosharraf Musa <31395110+MosharrafMusa@users.noreply.github.com>
2020-03-05 19:34:36 -0500

    MVP Reached

---
+++ src/Components/CardBuilder.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "./Card";
+import styled from "styled-components";
+
+const CardBuilder = props => {
+  const [data, updateData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=0eCAfzcU6VICeD5eWsXoPfhU6HQqG5T44OB21qdD&date=${
+          props.date
+        }`
+      )
+      .then(res => {
+        updateData(res.data);
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  }, [props.date]);
+  return (
+    <div>
+      <h1>Astronomy Picture of the Day</h1>
+      <Card
+        author={data.copyright}
+        explanation={data.explanation}
+        title={data.title}
+        date={data.date}
+        imgUrl={data.hdurl}
+      />
+    </div>
+  );
+};
+export default CardBuilder;
